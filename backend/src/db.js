9dadc7339cee7dac4c8e00e95ffaf0c4da354cce
Dionys Eicholz <dionysandriel@gmail.com>
2025-06-03 15:01:50 -0300

    Backend Login/Register

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,6 +5,11 @@ import dotenv from "dotenv";
 // Carrega variáveis de ambiente (.env)
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+    console.error("ERRO FATAL: DATABASE_URL não está definida no arquivo .env da pasta backend.");
+    process.exit(1); // Encerra a aplicação se o DB URL não estiver definido
+}
+
 export const sql = neon(process.env.DATABASE_URL);
 
 const requestHandler = async (req, res) => {
@@ -12,7 +17,7 @@ const requestHandler = async (req, res) => {
         const result = await sql`SELECT version()`;
         const { version } = result[0];
         res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end(version);
+        res.end(version); q
     } catch (error) {
         res.writeHead(500, { "Content-Type": "text/plain" });
         res.end("Erro ao conectar com o banco");
